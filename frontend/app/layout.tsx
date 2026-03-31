9125c7c8cf474890072657d1b43c013e3aca0607
Ubaidullah Ismail <abdulahadismail.dev@gmail.com>
2026-03-31 08:42:56 -0700

    Setup Authentication

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,13 @@
 import type { Metadata } from "next";
-import { Inter, Geist_Mono } from "next/font/google";
+import { Inter, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 // import { TRPCReactProvider } from "@/trpc/client";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const outfit = Outfit({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -33,7 +27,7 @@ export default function RootLayout({
     <ClerkProvider>
       {/* <TRPCReactProvider> */}
       <html lang="en">
-        <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
+        <body className={`${outfit.className} antialiased`}>
           <NuqsAdapter>{children}</NuqsAdapter>
           <Toaster />
         </body>
